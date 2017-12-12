85b2947421151dc5cc3301eab3b722653751e56b
Tom Barnes <ruralmyth@gmail.com>
2017-12-12 00:19:58 +0000

    more pleasant to read with extension function

--- src/main/kotlin/RunLengthEncoding.kt
+++ src/main/kotlin/RunLengthEncoding.kt
@@ -8,28 +8,27 @@ object RunLengthEncoding {
 
     fun encode(s: String): String {
         var currentCharIndex = 0
-        var selectedCharIndex= 0
+        var selectedCharIndex = 0
         val encodedPairs: MutableList<Pair<Int, Char>> = mutableListOf()
         while (currentCharIndex < s.length) {
             var count = 0
             while (selectedCharIndex < s.length && s[selectedCharIndex] == s[currentCharIndex]) {
-                count ++
-                selectedCharIndex ++
+                count++
+                selectedCharIndex++
             }
             if (count > 0) {
                 encodedPairs.add(count to s[currentCharIndex])
             }
-            currentCharIndex ++
+            currentCharIndex++
         }
         return encodedPairs
-                .map { "${countIfNotOne(it.first)}${it.second}" }
+                .map { "${it.first.ifNotOne()}${it.second}" }
                 .joinToString("")
     }
 
-    private fun countIfNotOne(count: Int) = count.let {
-        when (count) {
+    private fun Int.ifNotOne() =
+        when (this) {
             1 -> ""
-            else -> count.toString()
+            else -> this.toString()
         }
-    }
 }
