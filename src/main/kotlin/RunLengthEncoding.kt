688cdf5b2051fb660be7ded5a90cb2da340b06ff
Tom Barnes <ruralmyth@gmail.com>
2017-12-12 00:13:04 +0000

    can 'encode string with no single characters', but there must be a nicer way.

--- src/main/kotlin/RunLengthEncoding.kt
+++ src/main/kotlin/RunLengthEncoding.kt
@@ -2,7 +2,39 @@ object RunLengthEncoding {
     operator fun invoke(string: String) = string.let {
         when {
             it.isEmpty() -> ""
-            else -> it
+            else -> encode(it)
+        }
+    }
+
+    fun encode(s: String): String {
+        var currentCharIndex = 0
+        var selectedCharIndex= 0
+        val encodedPairs: MutableList<Pair<Int, Char>> = mutableListOf()
+        while (currentCharIndex < s.length) {
+            var count = 0
+            while (selectedCharIndex < s.length && s[selectedCharIndex] == s[currentCharIndex]) {
+                count ++
+                selectedCharIndex ++
+            }
+            encodedPairs.add(count to s[currentCharIndex])
+            currentCharIndex ++
+        }
+        return encodedPairs
+                .map { "${countIfNotZeroOrOne(it.first)}${charIfNotZero(it.first, it.second)}" }
+                .joinToString("")
+    }
+
+    private fun charIfNotZero(count: Int, char: Char) = count.let {
+        when (count) {
+            0 -> ""
+            else -> char.toString()
+        }
+    }
+
+    private fun countIfNotZeroOrOne(count: Int) = count.let {
+        when (count) {
+            0, 1 -> ""
+            else -> count.toString()
         }
     }
 }
--- src/test/kotlin/RunLengthEncodingTest.kt
+++ src/test/kotlin/RunLengthEncodingTest.kt
@@ -1,6 +1,5 @@
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.junit.Ignore
 import org.junit.Test
 
 class RunLengthEncodingTest {
@@ -13,5 +12,10 @@ class RunLengthEncodingTest {
     @Test
     fun `encode single characters without count`() {
         assertThat(RunLengthEncoding("XYZ"), equalTo("XYZ"))
+    }
+
+    @Test
+    fun `encode string with no single characters`() {
+        assertThat(RunLengthEncoding("AABBBCCCC"), equalTo("2A3B4C"))
     }
 }
