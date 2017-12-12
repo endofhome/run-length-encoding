58ab74e305dd56fd51faaba5a120810e2623dca4
Tom Barnes <ruralmyth@gmail.com>
2017-12-12 00:17:28 +0000

    ever so slightly simpler.

--- src/main/kotlin/RunLengthEncoding.kt
+++ src/main/kotlin/RunLengthEncoding.kt
@@ -16,24 +16,19 @@ object RunLengthEncoding {
                 count ++
                 selectedCharIndex ++
             }
-            encodedPairs.add(count to s[currentCharIndex])
+            if (count > 0) {
+                encodedPairs.add(count to s[currentCharIndex])
+            }
             currentCharIndex ++
         }
         return encodedPairs
-                .map { "${countIfNotZeroOrOne(it.first)}${charIfNotZero(it.first, it.second)}" }
+                .map { "${countIfNotOne(it.first)}${it.second}" }
                 .joinToString("")
     }
 
-    private fun charIfNotZero(count: Int, char: Char) = count.let {
+    private fun countIfNotOne(count: Int) = count.let {
         when (count) {
-            0 -> ""
-            else -> char.toString()
-        }
-    }
-
-    private fun countIfNotZeroOrOne(count: Int) = count.let {
-        when (count) {
-            0, 1 -> ""
+            1 -> ""
             else -> count.toString()
         }
     }
