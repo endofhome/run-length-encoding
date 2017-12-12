eafdc43e4ca6dcc91a9f067494aac4337e663607
Tom Barnes <ruralmyth@gmail.com>
2017-12-12 08:39:20 +0000

    encode string with whitespace characters mixed in it

--- src/test/kotlin/RunLengthEncodingTest.kt
+++ src/test/kotlin/RunLengthEncodingTest.kt
@@ -23,4 +23,9 @@ class RunLengthEncodingTest {
     fun `encode string with single and mixed characters`() {
         assertThat(RunLengthEncoding("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"), equalTo("12WB12W3B24WB"))
     }
+
+    @Test
+    fun `encode string with whitespace characters mixed in it`() {
+        assertThat(RunLengthEncoding("  hsqq qww  "), equalTo("2 hs2q q2w2 "))
+    }
 }
