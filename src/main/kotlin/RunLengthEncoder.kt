5666e70ef6b10633b0addd3f8637f057175c2fc7
Tom Barnes <ruralmyth@gmail.com>
2017-12-14 00:03:37 +0000

    refactor to smaller methods

--- src/main/kotlin/RunLengthEncoder.kt
+++ src/main/kotlin/RunLengthEncoder.kt
@@ -7,22 +7,25 @@ object RunLengthEncoder {
     }
 
     private fun encode(s: String, processed: String): String {
+        val partitionedString = partition(s)
+        val remainderExists = partitionedString.remainder.isNotEmpty()
+        val numberOfRepeats = partitionedString.repeatedChars.length.display()
+        val characterToRepeat = partitionedString.repeatedChars.first()
+        val encoded = "$processed$numberOfRepeats$characterToRepeat"
+        return when {
+            remainderExists -> encode(partitionedString.remainder.toString(), encoded)
+            else -> encoded
+        }
+    }
+
+    private fun partition(s: String): Partition {
         val nextChar = s.find { it != s.first() }
-        val partitionedString: Partition = nextChar?.let { next ->
+        return nextChar?.let { next ->
             val nextCharIndex = s.indexOfFirst { it == next }
             val repeated = s.subSequence(0, nextCharIndex)
             val remainder = s.subSequence(nextCharIndex, s.lastIndex + 1)
             Partition(repeated, remainder)
         } ?: Partition(s, "")
-
-        val numberOfRepeats = partitionedString.repeatedChars.length.display()
-        val character = partitionedString.repeatedChars.first()
-        val encoded = "$processed$numberOfRepeats$character"
-        val remainderExists = partitionedString.remainder.isNotEmpty()
-        return when {
-            remainderExists -> encode(partitionedString.remainder.toString(), encoded)
-            else -> encoded
-        }
     }
 
     private fun Int.display() = when (this) {
