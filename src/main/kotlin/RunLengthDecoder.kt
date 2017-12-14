bd9f0f989050d4fed6281a312608f52d200ec108
Tom Barnes <ruralmyth@gmail.com>
2017-12-14 23:52:35 +0000

    formatting

--- src/main/kotlin/RunLengthDecoder.kt
+++ src/main/kotlin/RunLengthDecoder.kt
@@ -2,7 +2,7 @@ object RunLengthDecoder {
     operator fun invoke(s: String) = s.let {
         when {
             it.isEmpty() -> ""
-            else -> decode(it)
+            else         -> decode(it)
         }
     }
 
